30e2d61fdbaa0da44bc7d58a69fb42ed855bdd85
abyad-tif <lapajung50@gmail.com>
2025-11-05 11:56:20 +0800

    menambahkan fungsi pada role admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -9,19 +9,55 @@ const { body, validationResult } = require("express-validator");
 
 const prisma = require("../utils/db");
 
-// Fungsi Menampilkan Data
+// Fungsi Menampilkan Semua Data
 router.get("/", verifyToken, authorizeRole, async function (req, res) {
   try {
-    const user = await prisma.user.findMany();
+    // const user = await prisma.user.findMany();
+    const alumni = await prisma.alumni.findMany();
+    const pendidikan = await prisma.pendidikan.findMany();
+    const pekerjaan = await prisma.pekerjaan.findMany();
 
     return res.json({
       status: 200,
-      message: "Data Semua Users",
-      data: user,
+      message: "Semua Data",
+      alumni: alumni,
+      pendidikan: pendidikan,
+      pekerjaan: pekerjaan,
     });
   } catch (e) {
     console.error(`Error fetching data: ${e}`);
   }
 });
+
+// Fungsi Menampilkan Data Berdasarkan Email
+router.post(
+  "/getDataSpecificUser",
+  verifyToken,
+  authorizeRole,
+  async function (req, res) {
+    try {
+      // const user = await prisma.user.findMany();
+      const alumni = await prisma.alumni.findUnique({
+        where: { alumni_email: req.body.email },
+      });
+      const pendidikan = await prisma.pendidikan.findMany({
+        where: { alumni_email: req.body.email },
+      });
+      const pekerjaan = await prisma.pekerjaan.findMany({
+        where: { alumni_email: req.body.email },
+      });
+
+      return res.json({
+        status: 200,
+        message: "Semua Data",
+        alumni: alumni,
+        pendidikan: pendidikan,
+        pekerjaan: pekerjaan,
+      });
+    } catch (e) {
+      console.error(`Error fetching data: ${e}`);
+    }
+  }
+);
 
 module.exports = router;
---
+++ src/routes/loker.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const router = express.Router();
+
+const { body, validationResult } = require("express-validator");
+
+const { verify } = require("crypto");
+const verifyToken = require("../middleware/token");
+const prisma = require("../utils/db");
+
+// Fungsi Menampilkan Data - BetterSqlite3
+router.get("/", verifyToken, async function (req, res) {
+  try {
+    const loker = await prisma.loker.findMany();
+
+    return res.json({
+      status: 200,
+      message: "Data Loker",
+      data: loker,
+    });
+  } catch (e) {
+    console.error(`Error fetching data: ${e}`);
+  }
+});
+
+let lokerValidation = [
+  body("judul").notEmpty(),
+  body("deskripsi").notEmpty(),
+  body("nama_perusahaan").notEmpty(),
+  body("industri").notEmpty(),
+  body("alamat").notEmpty(),
+  body("tgl_terbit").notEmpty(),
+  body("tgl_kadaluarsa").notEmpty(),
+];
+
+// Fungsi Memasukkan Data - Non User Login
+router.post("/store", lokerValidation, async function (req, res) {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).json({
+      errors: error.array(),
+    });
+  }
+
+  try {
+    await prisma.loker.create({
+      data: {
+        judul: req.body.judul,
+        deskripsi: req.body.deskripsi,
+        nama_perusahaan: req.body.nama_perusahaan,
+        industri: req.body.industri,
+        alamat: req.body.alamat,
+        tgl_terbit: req.body.tgl_terbit,
+        tgl_kadaluarsa: req.body.tgl_kadaluarsa,
+      },
+    });
+
+    return res.json({
+      status: 201,
+      message: "Data Berhasil Ditambahkan",
+    });
+  } catch (e) {
+    console.error(`Error: ${e}`);
+  }
+});
+
+// Fungsi Memasukkan Data - User login
+router.patch(
+  "/update",
+  lokerValidation,
+  verifyToken,
+  async function (req, res) {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(422).json({
+        errors: error.array(),
+      });
+    }
+
+    try {
+      await prisma.loker.update({
+        where: { alumni_email: req.user.email },
+        data: {
+          judul: req.body.judul,
+          deskripsi: req.body.deskripsi,
+          nama_perusahaan: req.body.nama_perusahaan,
+          industri: req.body.industri,
+          alamat: req.body.alamat,
+          tgl_terbit: req.body.tgl_terbit,
+          tgl_kadaluarsa: req.body.tgl_kadaluarsa,
+        },
+      });
+
+      return res.json({
+        status: 201,
+        message: "Data berhasil diubah",
+      });
+    } catch (e) {
+      console.error(`Error inserting data: ${e}`);
+    }
+  }
+);
+
+module.exports = router;
